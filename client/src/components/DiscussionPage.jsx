906b185831956a47e54bd82d95b3423675d11c33
Santy520 <sspalacio20@gmail.com>
2024-05-28 21:29:34 -0400

    created a discussion page and created a client folder

---
+++ client/src/components/DiscussionPage.jsx
@@ -0,0 +1,19 @@
+function DiscussionPage() {
+
+
+    return (
+        <body>
+            
+  <div className="about-me-section">
+    <h1 className="body">Discussion Page </h1>
+    <p className="about-me-text">
+      Write your discussions in here.
+    </p>
+  </div>
+
+</body> 
+        
+    )
+}
+
+export default DiscussionPage
