35d042b24e60e7d8d64b955a3d3a7d031d2e3f30
Crystal D <crystaldepalma@yahoo.com>
2024-05-30 20:27:36 -0400

    added comments and neatened up resolvers, created a root level gitignore

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,14 +3,8 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/book-club');
 
-const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-    console.log('Connected to MongoDB');
-});
-
-module.exports = db;
+module.exports = mongoose.connection;
 
 
 // useNewUrlParser: true,
