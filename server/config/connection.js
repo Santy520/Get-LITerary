e9e8954f487d6438f599b5554d751a017d0393a5
Gibin <gibinmgeorge2001@gmail.com>
2024-05-30 17:14:51 -0700

    added routes and controller basics

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,20 +1,5 @@
-
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/book-club');
 
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-    console.log('Connected to MongoDB');
-});
-
-module.exports = db;
-
-
-// useNewUrlParser: true,
-// useUnifiedTopology: true,
-// useCreateIndex: true,
-// useFindAndModify: false
-    // check out these options to add on line 5, extending from line 4
+module.exports = mongoose.connection;
