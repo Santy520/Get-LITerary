f696b70975042ef5d52307b4c7af30a3c85321ce
Gibin <gibinmgeorge2001@gmail.com>
2024-06-01 14:55:29 -0700

    added gql queries and mutations, also deleted unwanted files

--- server/routes/api/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const bookRoutes = require('./bookRoutes');
-
-// Use the book routes
-router.use('/books', bookRoutes);
-
-module.exports = router;
