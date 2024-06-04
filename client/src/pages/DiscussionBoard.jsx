1176728f9cea2ee4ae8d162b403a7e7b042d9b3f
Santy520 <sspalacio20@gmail.com>
2024-06-04 19:58:03 -0400

    got rid of suscribe in the discussion board, and fixed connection issues with pages

--- client/src/pages/DiscussionBoard.jsx
+++ client/src/pages/DiscussionBoard.jsx
@@ -2,36 +2,29 @@ import { useState } from 'react';
 import PostForm from '../components/PostForm';
 import Post from '../components/Post';
 
-  function DiscussionBoard() {
-    const [posts, setPosts] = useState([]);
-    const [subscribed, setSubscribed] = useState(false);
-  
-    const addPost = (text) => {
-      setPosts([...posts, { id: Date.now(), text }]);
-    };
-  
-    const deletePost = (id) => {
-      setPosts(posts.filter(post => post.id !== id));
-    };
-  
-    const toggleSubscribe = () => {
-      setSubscribed(!subscribed);
-    };
-  
-    return (
-      <div className="discussion-board">
-        <h1>Discussion Board</h1>
-        {/* <button className="subscribe-button" onClick={toggleSubscribe}>
-          {subscribed ? 'Unsubscribe' : 'Subscribe'}
-        </button> */}
-        <PostForm addPost={addPost} />
-        <div className="posts">
-          {posts.map(post => (
-            <Post key={post.id} post={post} deletePost={deletePost} />
-          ))}
-        </div>
+function DiscussionBoard() {
+  const [posts, setPosts] = useState([]);
+
+  const addPost = (text) => {
+    setPosts([...posts, { id: Date.now(), text }]);
+  };
+
+  const deletePost = (id) => {
+    setPosts(posts.filter(post => post.id !== id));
+  };
+
+  return (
+    <div className="discussion-board">
+      <h1>Discussion Board</h1>
+      {/* The subscribe button is removed */}
+      <PostForm addPost={addPost} />
+      <div className="posts">
+        {posts.map(post => (
+          <Post key={post.id} post={post} deletePost={deletePost} />
+        ))}
       </div>
-    );
-  }
-  
-  export default DiscussionBoard;
+    </div>
+  );
+}
+
+export default DiscussionBoard;
